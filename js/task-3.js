79d65dd472a7b83b31a85279ce29b42ad4872d5a
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-07-27 08:27:42 +0300

    create gallery

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const input = document.getElementById('name-input');
-const output = document.getElementById('name-output');
-input.addEventListener('input', () => {
-  const trimmedValue = input.value.trim();
-  output.textContent = trimmedValue ? trimmedValue : 'Anonymous';
-});
